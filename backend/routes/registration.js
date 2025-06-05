f417363b1b324c21c43e6ae3fd9124ee525b55de
LokeshDevCreates <lokil3832@gmail.com>
2025-06-05 10:54:12 +0530

    Update registration.js

--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Registration = require('../models/registration');
+const Registration = require('../models/registrationModel');
 
 // POST - Save registration
 router.post('/', async (req, res) => {
